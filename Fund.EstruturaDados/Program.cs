c4996d3fe1fa6ed8ddbb226467b2094f6ddc212a
Deyvisson-del <deyvissonrafael018@gmail.com>
2026-02-17 19:59:16 -0300

    Aprendendo estrutura de Dados: Tempo de execução, como calcular o tempo de execução de um algoritmo

---
+++ Fund.EstruturaDados/Program.cs
@@ -0,0 +1,24 @@
+﻿   public class Program
+   {
+      static void Main(string[] args)
+      {
+         int[] A = { 5, 2, 8, 1, 9 };
+         int n = A.Length;
+         int menor = CalcularMenor(A, n);
+        Console.WriteLine("O menor valor é: " + menor);
+      }
+
+    public static int CalcularMenor(int[] A, int n)
+    {
+        int menor, i;
+        menor = A[0];
+        for (i = 1; i < n; i++)
+        {
+            if (A[i] < menor)
+            {
+                menor = A[i];
+            }
+        }
+        return menor;
+    }
+   }
